e4f1dc6b5b7381101d8f7325296972e6f697a3ce
Andrew Marshall <planetmarshalluk@gmail.com>
2024-04-16 23:22:18 +0100

    initial commit

---
+++ download-llvm.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -ue -o pipefail
+
+COMMIT_SHA=ea3d0db130b9a6c4678c11dd8bc48e5630624b62
+URL=https://github.com/llvm/llvm-project/archive/${COMMIT_SHA}.tar.gz
+TARBALL="${COMMIT_SHA}.tar.gz"
+TARBALL_SHA256=61885612507cd439d0d08305ff6c2a60cf0923e7b046ea3a447f1b78d92de957
+
+wget --quiet ${URL}
+echo "${TARBALL_SHA256} ${TARBALL}" | sha256sum --check
+tar -xf "${TARBALL}"
+rm ${TARBALL}
+mv "llvm-project-${COMMIT_SHA}" llvm-project
