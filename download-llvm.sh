d5e5e2a3f546524357b0b30111839f268b66f813
Andrew Marshall <planetmarshalluk@gmail.com>
2024-04-17 15:57:57 +0100

    add test properties to config file

--- download-llvm.sh
+++ download-llvm.sh
@@ -7,7 +7,7 @@ URL=https://github.com/llvm/llvm-project/archive/${COMMIT_SHA}.tar.gz
 TARBALL="${COMMIT_SHA}.tar.gz"
 TARBALL_SHA256=61885612507cd439d0d08305ff6c2a60cf0923e7b046ea3a447f1b78d92de957
 if [[ "$(uname -s)" == "Darwin" ]]; then
-  shasum256() {
+  sha256sum() {
     shasum --algorithm 256 "$@"
   }
 fi
@@ -15,7 +15,7 @@ fi
 if ! [[ -f "${TARBALL}" ]]; then
   wget --quiet ${URL}
 fi
-echo "${TARBALL_SHA256}  ${TARBALL}" | shasum256 --check
+echo "${TARBALL_SHA256}  ${TARBALL}" | sha256sum --check
 tar -xf "${TARBALL}"
 rm ${TARBALL}
 mv "llvm-project-${COMMIT_SHA}" llvm-project
