90e0169b2f8dd547d46a6ce729b6eeb30087f4ad
Andrew Marshall <planetmarshalluk@gmail.com>
2024-04-19 09:32:57 +0100

    create more reliable breakpoint test

--- test/test_lldb_python.py
+++ test/test_lldb_python.py
@@ -1,7 +1,14 @@
-import os
-
 import lldb
 from lldb import SBDebugger, SBTarget, SBProcess, SBError, SBBreakpoint
+
+
+def _launch_process(target: SBTarget) -> SBProcess:
+    error = SBError()
+    process: SBProcess = target.Launch(target.debugger.GetListener(), None, None,
+                                        None, None, None,
+                                        None, 0, False, error)
+    assert error.success, error.description
+    return process
 
 
 def test_create_debugger():
@@ -19,12 +26,8 @@ def test_create_target(executable):
 def test_launch_process(executable):
     debugger: SBDebugger = SBDebugger.Create()
     target: SBTarget = debugger.CreateTargetWithFileAndArch(executable, lldb.LLDB_ARCH_DEFAULT)
-    error = SBError()
-    process: SBProcess = target.Launch(debugger.GetListener(), None, None,
-                                        None, None, None,
-                                        None, 0, False, error)
+    process: SBProcess = _launch_process(target)
 
-    assert error.success, error.description
     assert process is not None
     assert process.id != 0
 
@@ -34,8 +37,11 @@ def test_launch_process_with_breakpoint(executable):
     debugger.SetAsync(False)
     target: SBTarget = debugger.CreateTargetWithFileAndArch(executable, lldb.LLDB_ARCH_DEFAULT)
     bp: SBBreakpoint = target.BreakpointCreateByName("main", target.GetExecutable().GetFilename())
+
     assert bp is not None
-    process: SBProcess = target.LaunchSimple(None, None, os.getcwd())
+    assert bp.num_locations == 1
+    process: SBProcess = _launch_process(target)
+
     assert process.is_stopped
-    assert process.id != 0
+
     process.Continue()
