fa872f6abdb40ca16546bacc7643d6c57a96e871
Andrew Marshall <planetmarshalluk@gmail.com>
2024-12-16 19:06:50 +0000

    update ccache version

--- download-install-ccache.sh
+++ download-install-ccache.sh
@@ -3,10 +3,10 @@
 set -eux -o pipefail
 
 INSTALL_DIR=$(realpath "${1:-/usr/local/bin}")
-CCACHE_VERSION=4.9.1
+CCACHE_VERSION=4.10.2
 CCACHE_BASENAME=ccache-${CCACHE_VERSION}-linux-x86_64
 CCACHE_TARBALL=${CCACHE_BASENAME}.tar.xz
-CCACHE_SHA256=d33f17ca6e02918cdc44738ce3adacb586bca65374ff1ceb0c4b2f1c87a24bef
+CCACHE_SHA256=80cab87bd510eca796467aee8e663c398239e0df1c4800a0b5dff11dca0b4f18
 CCACHE_URL=https://github.com/ccache/ccache/releases/download/v${CCACHE_VERSION}/${CCACHE_TARBALL}
 pushd /tmp || exit
 wget --quiet ${CCACHE_URL}
